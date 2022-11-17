4fd0e505740687dc6d367e1900a20021c36695c8
Shreyas <shreyasjavli2001@gmail.com>
2022-11-17 18:53:44 +0530

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+print("shreyas is  a good boy")
+###git init
+#git add README.md
+##git commit -m "first commit"
+#git branch -M main
+##git remote add origin https://github.com/ShreyasDJ2001/fgfgf.git
+#git push -u origin main
